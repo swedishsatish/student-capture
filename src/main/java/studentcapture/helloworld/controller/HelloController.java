3a76967f39c82dfeda880b15b498db7bd20893d7
Victor Zars <c13vzs@cs.umu.se>
2016-04-22 13:14:30 +0200

    Just testing to push.
    Added a TestModel to test.

--- src/main/java/studentcapture/helloworld/controller/HelloController.java
+++ src/main/java/studentcapture/helloworld/controller/HelloController.java
@@ -6,20 +6,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import studentcapture.helloworld.model.HelloModel;
+import studentcapture.helloworld.model.TestModel;
 
 @RestController
 public class HelloController {
 	private HelloModel model = new HelloModel();
-	
+	private TestModel testModel = new TestModel();
+
 	@RequestMapping(value = "hello", method = RequestMethod.GET)
 	public HelloModel hello() {
 		return model;
 	}
-	
+
 	@RequestMapping(value = "hello", method = RequestMethod.POST)
 	public void hello(@RequestParam(value="greeting", required=false) String greeting) {
 		if (greeting != null) {
 			model.setGreeting(greeting);
+		}
+	}
+
+	@RequestMapping(value = "message", method = RequestMethod.GET)
+	public TestModel message() { return testModel; }
+
+	@RequestMapping(value = "message", method = RequestMethod.POST)
+	public void message(@RequestParam(value="message", required = false) String message){
+		if(message != null){
+			model.setMessage(message);
 		}
 	}
 }
