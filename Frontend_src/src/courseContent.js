062f87c2c677ab44ae3abb77917c8be4a151d92a
System Administrator <root@client216-59.wireless.umu.se>
2016-04-26 13:37:54 +0200

    add css shared by every page ÄNDRA INGET I DE HÄR FILERNA TACK

---
+++ Frontend_src/src/courseContent.js
@@ -0,0 +1,33 @@
+var UserGist = React.createClass({
+  getInitialState: function() {
+    return {
+      username: '',
+      lastGistUrl: ''
+    };
+  },
+
+  componentDidMount: function() {
+    this.serverRequest = $.get(this.props.source, function (result) {
+      var lastGist = result[0];
+      this.setState({
+        username: lastGist.owner.login,
+        lastGistUrl: lastGist.html_url
+      });
+    }.bind(this));
+  },
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this.state.username}'s last gist is
+        <a href={this.state.lastGistUrl}>here</a>.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<UserGist source="https://api.github.com/users/octocat/gists" />, document.getElementById('courseContent'));
---
+++ Frontend_src/src/footer.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+    <p id="legalText">&copy;TUPAC AND BIGGIE DIED 2 YOUNG&copy;</p>,
+    document.getElementById('footer')
+);
