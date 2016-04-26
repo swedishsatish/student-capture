db34a33c17459e37566bfd9e86077b2a4cd37e07
Olle <c12osn@cs.umu.se>
2016-04-26 09:16:38 +0200

    implemented support for listening to a post request at /feedback/postInfo

--- src/main/java/studentcapture/feedback/FeedbackFetchingController.java
+++ src/main/java/studentcapture/feedback/FeedbackFetchingController.java
@@ -1,8 +1,25 @@
 package studentcapture.feedback;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by c12osn on 2016-04-26.
  */
+
+@RestController
+@RequestMapping(value = "feedback")
 public class FeedbackFetchingController {
 
-}
+    @RequestMapping(value = "postInfo", method = RequestMethod.POST)
+    public void handleFeedbackRequestFromStudent() {
+
+    }
+
+
+
+
+
+
+    }
