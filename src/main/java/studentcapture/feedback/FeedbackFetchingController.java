af1d9cf2b1431d6149d040b798b56844af4d7639
Olle <c12osn@cs.umu.se>
2016-04-26 09:09:43 +0200

    created first test for feedbackFetching functionality

---
+++ src/main/java/studentcapture/feedback/FeedbackFetchingController.java
@@ -0,0 +1,8 @@
+package studentcapture.feedback;
+
+/**
+ * Created by c12osn on 2016-04-26.
+ */
+public class FeedbackFetchingController {
+
+}
---
+++ src/test/java/studentcapture/feedback/FeedbackFetchingControllerTest.java
@@ -0,0 +1,37 @@
+package studentcapture.feedback;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+import studentcapture.config.StudentCaptureApplicationTests;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by c12osn on 2016-04-26.
+ */
+public class FeedbackFetchingControllerTest extends StudentCaptureApplicationTests {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+
+    @Test
+    public void shouldBeAbleToMakeSimplePostRequest() throws Exception {
+        mockMvc.perform(post("/feedback/postInfo"))
+                .andExpect(status().isOk());
+    }
+}
