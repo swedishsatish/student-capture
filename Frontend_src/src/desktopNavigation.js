a6e38533c911e64e1643669db82736a5eb04cc78
System Administrator <root@Filips-MacBook-Pro-2.local>
2016-04-26 17:38:20 +0200

    add more CSS. COURSECONTENT.JS CHANGED TOO

--- Frontend_src/src/desktopNavigation.js
+++ Frontend_src/src/desktopNavigation.js
@@ -11,7 +11,22 @@ var courses = [
 	{name:'course 1', id:'1'},
 	{name:'course 2', id:'2'},
 	{name:'course 3', id:'3'},
-	{name:'course 4', id:'4'}
+	{name:'course 3', id:'4'},
+	{name:'course 3', id:'5'},
+	{name:'course 3', id:'6'},
+	{name:'course 3', id:'7'},
+	{name:'course 3', id:'8'},
+	{name:'course 3', id:'9'},
+	{name:'course 3', id:'10'},
+	{name:'course 3', id:'11'},
+	{name:'course 3', id:'12'},
+	{name:'course 3', id:'13'},
+	{name:'course 4', id:'14'},
+	{name:'course 3', id:'15'},
+	{name:'course 3', id:'16'},
+	{name:'course 3', id:'17'},
+	{name:'course 3', id:'18'},
+	{name:'course 3', id:'19'},
 ];
 
 ReactDOM.render(<CourseList courses = {courses} />, document.getElementById('desktopNavigation'));
