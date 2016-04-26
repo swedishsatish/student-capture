ebfd529e4bc5a5246c88b51daa4b23c3df7c95bb
c13bll <c13bll@cs.umu.se>
2016-04-26 13:49:49 +0200

    Test for UserData

--- src/main/java/studentcapture/video/UserData.java
+++ src/main/java/studentcapture/video/UserData.java
@@ -5,4 +5,35 @@ package studentcapture.video;
  */
 public class UserData {
 
+    private String userID;
+    private Long courseID;
+    private String examID;
+
+
+    public UserData addExamID(String examID) {
+        this.examID = examID;
+        return this;
+    }
+
+    public UserData addCourseID(Long courseID) {
+        this.courseID = courseID;
+        return this;
+    }
+
+    public UserData addUserID(String userID){
+        this.userID = userID;
+        return this;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public Long getCourseID() {
+        return courseID;
+    }
+
+    public String getExamID() {
+        return examID;
+    }
 }
