fa74918313bf93a2fd21736ae900c98ad187c9c2
Gustav Andersson <c13gan@cs.umu.se>
2016-04-26 13:00:10 +0200

    Database communicator with a simple test

---
+++ src/test/java/studentcapture/datalayercommunicator/DatabaseCommunicatorTest.java
@@ -0,0 +1,18 @@
+package studentcapture.datalayercommunicator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by c13gan on 2016-04-26.
+ */
+public class DatabaseCommunicatorTest {
+
+    DatabaseCommunicator dlc = new DatabaseCommunicator();
+    @Test
+    public void testReturnGrade() throws Exception {
+        //dlc.insertTestValues();
+        assertEquals("vg",dlc.returnGrade(1337, 10));
+    }
+}
