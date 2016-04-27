cb11378dbcf240811f89a5c53dd8a049097e139e
Sanna <sanna.agren@gmail.com>
2016-04-27 16:24:32 +0200

    Some setup for Spring

--- src/test/java/studentcapture/datalayer/database/AssignmentTest.java
+++ src/test/java/studentcapture/datalayer/database/AssignmentTest.java
@@ -1,27 +1,66 @@
 package studentcapture.datalayer.database;
 
-/**
- * Created by c12ton on 4/26/16.
- */
-public class AssignmentTest {
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import studentcapture.config.StudentCaptureApplicationTests;
 
-    @org.junit.Test
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by S&E on 4/27/16.
+ */
+public class AssignmentTest extends StudentCaptureApplicationTests {
+
+    @Autowired
+    Assignment ass;
+    ArrayList actual;
+
+    /*@Test
     public void testCreateAssignment() throws Exception {
 
+    }*/
+
+    @Test
+    public void testGetAssignmentReturnListOfCorrectLength() throws Exception {
+        actual = ass.getAssignmentInfo(10);
+
+        assertEquals(6, actual.size());
     }
 
-    @org.junit.Test
-    public void testGetAssignmentInfo() throws Exception {
+    @Test
+    public void testGetAssignmentReturnsNotNUll() {
 
+        actual = ass.getAssignmentInfo(10);
+
+        assertFalse(actual.contains(null));
     }
 
-    @org.junit.Test
+    @Test
+    public void testGetAssignmentReturnsCorrectInfo() {
+        ArrayList expected = new ArrayList<String>();
+        expected.add("enkurs");
+        expected.add("titel");
+        expected.add("datum");
+        expected.add("datum2");
+        expected.add("tid");
+        expected.add("tid3");
+
+        actual = ass.getAssignmentInfo(10);
+
+        //assertEquals(actual, expected);
+    }
+
+    /*@Test
     public void testUpdateAssignment() throws Exception {
 
     }
 
-    @org.junit.Test
+    @Test
     public void testRemoveAssignment() throws Exception {
 
-    }
+    }*/
 }
