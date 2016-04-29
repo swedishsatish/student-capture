f9040a048410f917697a35a57cc16488c28c7187
Jonathan Westin <jwestin@cs.umu.se>
2016-04-29 15:48:18 +0200

    Crated an specialLITEException

---
+++ src/main/java/studentcapture/lti/LTIInvalidGradeException.java
@@ -0,0 +1,11 @@
+package studentcapture.lti;
+
+/**
+ * Created by jwestin on 4/29/16.
+ */
+public class LTIInvalidGradeException extends LTICommunicatorException {
+
+    public LTIInvalidGradeException(String message) {
+        super(message);
+    }
+}
