3537054dc2a44307493f9a11c1c219ebfe113199
dv14jbm <dv14jbm@cs.umu.se>
2016-04-29 10:32:56 +0200

    Implemented getGrade

--- src/main/java/studentcapture/datalayer/DatalayerCommunicator.java
+++ src/main/java/studentcapture/datalayer/DatalayerCommunicator.java
@@ -1,82 +1,110 @@
-
 package studentcapture.datalayer;
 
+import java.io.FileInputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import studentcapture.config.StudentCaptureApplication;
-import studentcapture.video.VideoInfo;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import studentcapture.datalayer.database.Assignment;
+import studentcapture.datalayer.database.Submission;
+import studentcapture.datalayer.filesystem.FilesystemInterface;
 
 /**
  * Created by c12osn on 2016-04-22.
+ * Edited by c13arm, ens13ahr
  */
 @RestController
 @RequestMapping(value = "DB")
 public class DatalayerCommunicator {
-//    @Autowired
-//private DatabaseCommunicator dbc;
-// Not that into what this stuff do, but
+
+
+
+    @Autowired
+    private Submission submission;
+    @Autowired
+    private Assignment assignment;
+
     @CrossOrigin()
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "getGrade", method = RequestMethod.POST)
-    public MultiValueMap getGrade(@RequestParam(value = "name", required = false) String name,
-                                  @RequestParam(value = "course", required = false) String course,
-                                  @RequestParam(value = "exam", required = false) String exam) {        // Creates the object that should be returned
-        LinkedMultiValueMap<String, String> returnData = new LinkedMultiValueMap<String, String>();        // Do your DB and filesystem calls
-        //DatabaseCommunicator dbc = new DatabaseCommunicator();
-        //String grade = dbc.returnGrade(1, 2);        // Add what you want to return to the map here
-        // EX: returndata.add("nyckel", variabel);
-        // EX: returndata.add("grade", grade);        // What is returned to the calling address
+    public MultiValueMap getGrade(@RequestParam(value = "studentID", required = false) String studentID,
+                                  @RequestParam(value = "courseCode", required = false) String courseCode,
+                                  @RequestParam(value = "courseID", required = false) String courseID,
+                                  @RequestParam(value = "assignmentID", required = false) String assignmentID) {
+
+        LinkedMultiValueMap<String, Object> returnData = new LinkedMultiValueMap<>();
+
+        returnData.add("grade", submission.getGrade(studentID, assignmentID).get("grade"));
+        returnData.add("time", submission.getGrade(studentID, assignmentID).get("time"));
+        returnData.add("teacher",  submission.getGrade(studentID, assignmentID).get("teacher"));
+
+
         return returnData;
     }
 
-    @CrossOrigin()
-    @RequestMapping(value = "/upload/question", method = RequestMethod.POST, headers = "content-type=multipart/form-data")
-    public ResponseEntity<String> uploadQuestion(@RequestBody VideoInfo videoToUpload) {
-        System.out.println("SUCCESS");
-        return new ResponseEntity<String>(HttpStatus.OK);
-    }
 
-    @CrossOrigin()
-    @RequestMapping(value = "/upload/answer", method = RequestMethod.POST, headers = "content-type=multipart/form-data")
-    public ResponseEntity<String> uploadAnswer(@RequestBody VideoInfo videoToUpload) {
-        System.out.println("SUCCESS");
-        return new ResponseEntity<String>(HttpStatus.OK);
-    }
+    /**
+     *
+     * @param courseID
+     * @param assignmentTitle
+     * @param startDate
+     * @param endDate
+     * @param minTime
+     * @param maxTime
+     * @param published
+     * @return
+     */
+    @CrossOrigin
+    @RequestMapping(value = "/createAssignment", method = RequestMethod.POST)
+    public int createAssignment(@RequestParam(value = "courseID") String courseID,
+                                @RequestParam(value = "assignmentTitle") String assignmentTitle,
+                                @RequestParam(value = "startDate") String startDate,
+                                @RequestParam(value = "endDate") String endDate,
+                                @RequestParam(value = "minTime") String minTime,
+                                @RequestParam(value = "maxTime") String maxTime,
+                                @RequestParam(value = "published") boolean published){
 
-    @CrossOrigin()
-    @RequestMapping(value = "/upload/feedback", method = RequestMethod.POST, headers = "content-type=multipart/form-data")
-    public ResponseEntity<String> uploadFeedback(@RequestBody VideoInfo videoToUpload) {
-        System.out.println("SUCCESS");
-        return new ResponseEntity<String>(HttpStatus.OK);
-    }
+        //int returnResult = ass.createAssignment(courseID, assignmentTitle, startDate, endDate, minTime, maxTime, published);
 
-    public ResponseEntity<String> uploadVideo(VideoInfo videoToUpload, String uploadPath) {
-        MultipartFile video = videoToUpload.getVideoFile();
-        if (!video.isEmpty()) {
-            try {
-                BufferedOutputStream stream = new BufferedOutputStream(
-                        new FileOutputStream(
-                                new File(uploadPath)));
-                FileCopyUtils.copy(video.getInputStream(), stream);
-                stream.close();
-            } catch (Exception e) {
-                System.err.println("Failed to upload file.");
-                return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
-            }
-        } else {
-            System.err.println("Bad file.");
-            return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
-        }
-        return new ResponseEntity(HttpStatus.CREATED);
+        return 1234;//returnResult;
+    }
+    /**
+     * Save grade for a submission
+     * @param assID
+     * @param teacherID
+     * @param studentID
+     * @param grade
+     * @return
+     */
+    @CrossOrigin
+    @RequestMapping(value = "/setGrade", method = RequestMethod.POST)
+    public boolean setGrade(@RequestParam(value = "assID") String assID,
+            				@RequestParam(value = "teacherID") String teacherID,
+            				@RequestParam(value = "studentID") String studentID,
+            				@RequestParam(value = "grade") String grade) {
+    	
+    	return false;
+    }
+    
+    /**
+     * Give feedback for a submission
+     * @param assID
+     * @param teacherID
+     * @param studentID
+     * @param feedbackVideo	Can be null
+     * @param feedbackText	Can be null
+     * @return
+     */
+    @CrossOrigin
+    @RequestMapping(value = "/giveFeedback", method = RequestMethod.POST)
+    public boolean giveFeedback(@RequestParam(value = "assID") String assID,
+            				@RequestParam(value = "teacherID") String teacherID,
+            				@RequestParam(value = "studentID") String studentID/*,
+            				@RequestParam(value = "feedbackVideo") video feedbackVideo,
+            				@RequestParam(value = "feedbackText") text feedbackText*/){
+    	
+    	return false;
     }
 }
