12f0de355f65ca543f2db189fb88c8146b6aacaf
c13arm <c13arm@cs.umu.se>
2016-05-02 10:59:04 +0200

    Created methods for teacher feedback

--- src/main/java/studentcapture/datalayer/filesystem/FilesystemConstants.java
+++ src/main/java/studentcapture/datalayer/filesystem/FilesystemConstants.java
@@ -1,10 +1,11 @@
 package studentcapture.datalayer.filesystem;
 
+
+import studentcapture.config.StudentCaptureApplication;
+
 public final class FilesystemConstants {
-	public final static String FILESYSTEM_PATH = "/home/c12/c12ton"; //TODO MooseFS directory
+	public final static String FILESYSTEM_PATH = StudentCaptureApplication.ROOT+"/moose"; //TODO MooseFS directory
 	public final static String SUBMISSION_VIDEO_FILENAME = "submission";
 	public final static String FEEDBACK_VIDEO_FILENAME = "feedback";
 	public final static String FEEDBACK_TEXT_FILENAME = "feedback.xml";
-	public final static String ASSIGNMENT_VIDEO_FILENAME = "video";
-    public final static String ASSIGNMENT_DESCRIPTION_FILENAME = "description.xml";
 }
