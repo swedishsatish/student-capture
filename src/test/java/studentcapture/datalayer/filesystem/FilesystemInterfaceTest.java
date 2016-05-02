6ddc4ce7c86815180c2eebfb4e533adcc5668e56
c13evk <c13ev@cs.umu.se>
2016-05-02 16:39:37 +0200

    Test for storing files

--- src/test/java/studentcapture/datalayer/filesystem/FilesystemInterfaceTest.java
+++ src/test/java/studentcapture/datalayer/filesystem/FilesystemInterfaceTest.java
@@ -2,11 +2,45 @@ package studentcapture.datalayer.filesystem;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import studentcapture.config.StudentCaptureApplication;
+import java.io.File;
 
 public class FilesystemInterfaceTest {
+    private String courseCode;
+    private String courseID;
+    private String assignmentID;
+    private String userID;
+    private MultipartFile testFile;
 
-	@Test
+    @Before
+    public void setUp() throws Exception {
+        courseCode = "test";
+        courseID = "5DV151";
+        assignmentID = "1337";
+        userID = "user";
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        deleteFile(new File(StudentCaptureApplication.ROOT+"/moose/"+courseCode));
+    }
+
+    @SuppressWarnings({"ResultOfMethodCallIgnored", "ConstantConditions"})
+    public static void deleteFile(File element) {
+        if (element.isDirectory()) {
+            for (File sub : element.listFiles()) {
+                deleteFile(sub);
+            }
+        }
+        element.delete();
+    }
+
+    @Test
 	public void testGeneratePathWithoutStudent() {
 		String path = FilesystemInterface.generatePath("5DV151", "1", "123");
 		
@@ -23,8 +57,42 @@ public class FilesystemInterfaceTest {
 	}
 
 	@Test
-	public void testSaveAFile() {
+	public void shouldStoreFileToNewPath(){
+		byte[] mockBytes = {1,2,4};
+        testFile = new MockMultipartFile("mockTestFile", mockBytes);
+		FilesystemInterface.storeStudentVideo(courseCode,courseID,assignmentID,userID,testFile);
 
+		File storedFile = new File(StudentCaptureApplication.ROOT+"/moose/"+courseCode+"/"+courseID+
+                "/"+userID+"/"+assignmentID+"/submission");
+        assertTrue(storedFile.exists());
 	}
+
+    @Test
+    public void shouldStoreFileToExistingPath(){
+        byte[] mockBytes = {1,2,4};
+        testFile = new MockMultipartFile("mockTestFile", mockBytes);
+        FilesystemInterface.storeStudentVideo(courseCode,courseID,assignmentID,userID,testFile);
+        testFile = new MockMultipartFile("mockTestFileExists", mockBytes);
+        FilesystemInterface.storeStudentVideo(courseCode,courseID,assignmentID,userID,testFile);
+        File storedFile = new File(StudentCaptureApplication.ROOT+"/moose/"+courseCode+"/"+courseID+
+                "/"+userID+"/"+assignmentID+"/submission");
+
+        assertTrue(storedFile.exists());
+    }
+
+    @Test
+    public void shouldCatchIOException(){
+        byte[] mockBytes = {1,2,4};
+        MultipartFile testFile = new MockMultipartFile("testNoWrite", mockBytes);
+        boolean exceptionCaught = FilesystemInterface.storeStudentVideo("test_no_write","5DV151","1337","user",
+                testFile);
+        assertFalse(exceptionCaught);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerException(){
+        FilesystemInterface.storeStudentVideo("test","5DV151","1337","user",null);
+
+    }
 
 }
