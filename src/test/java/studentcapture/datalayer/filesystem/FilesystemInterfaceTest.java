d65b0e058b1777c277fe7c2d66c0713833ba5660
Fredrik Mörtberg <c13fmg@cs.umu.se>
2016-05-02 09:35:58 +0200

    Changed to String values instead of int

--- src/test/java/studentcapture/datalayer/filesystem/FilesystemInterfaceTest.java
+++ src/test/java/studentcapture/datalayer/filesystem/FilesystemInterfaceTest.java
@@ -16,10 +16,15 @@ public class FilesystemInterfaceTest {
 	
 	@Test
 	public void testGeneratePathWithStudent() {
-		String path = FilesystemInterface.generatePath("5DV151", 1, 123, 654);
+		String path = FilesystemInterface.generatePath("5DV151", "1", "123", "654");
 		
 		assertEquals(path, FilesystemConstants.FILESYSTEM_PATH 
 					 + "/5DV151/1/123/654/");
+	}
+
+	@Test
+	public void testSaveAFile() {
+
 	}
 
 }
