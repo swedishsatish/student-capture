95ce74459038cc309fbb5aa7f4fac49cb9b2ab7e
Fredrik Mörtberg <c13fmg@cs.umu.se>
2016-05-02 09:16:24 +0200

    Updated constants and directory for "moose"

--- src/main/java/studentcapture/datalayer/filesystem/FilesystemConstants.java
+++ src/main/java/studentcapture/datalayer/filesystem/FilesystemConstants.java
@@ -1,7 +1,10 @@
 package studentcapture.datalayer.filesystem;
 
+
+import studentcapture.config.StudentCaptureApplication;
+
 public final class FilesystemConstants {
-	public final static String FILESYSTEM_PATH = ""; //TODO MooseFS directory
+	public final static String FILESYSTEM_PATH = StudentCaptureApplication.ROOT+"/moose"; //TODO MooseFS directory
 	public final static String SUBMISSION_VIDEO_FILENAME = "submission";
 	public final static String FEEDBACK_VIDEO_FILENAME = "feedback";
 	public final static String FEEDBACK_TEXT_FILENAME = "feedback.xml";
