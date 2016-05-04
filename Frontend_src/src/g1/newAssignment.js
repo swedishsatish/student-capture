5205f2d9257acd5ca20aa73ec2bf4bf81026e9a1
Filip Norman Golles <FilipGolles@client219-83.wireless.umu.se>
2016-05-04 08:53:01 +0200

    add group1 user story to demo.thml

---
+++ Frontend_src/src/g1/newAssignment.js
@@ -0,0 +1,82 @@
+var NewAssignment = React.createClass({
+    render : function() {
+      return <div>
+                <form id="form" action="assignment" method="post">
+                <input className="inputField" id="title" type="text" defaultValue="title" /><br/>
+                <input className="inputField" id="info" type="text" defaultValue="description" /><br/>
+                <p>VIDEO RECORDING COMPONENT GOES HERE</p>
+                <input id="startDate" type="datetime-local" /><br/>
+                <input id="endDate" type="datetime-local" /><br/>
+                <input id="minTimeSeconds" type="text" defaultValue="minTimeSeconds" /><br/>
+                <input id="maxTimeSeconds" type="text" defaultValue="maxTimeSeconds" /><br/>
+                <input id="isPublished" type="checkbox" value="Car"/>Publish Assignment<br/>
+                <div className="button primary-button" onClick = {handleCancel}> CANCEL </div>
+                <div className="button primary-button" onClick = {submitAssignment}> SUBMIT </div>
+            </form>
+        </div>
+    }
+});
+
+function handleCancel() {
+
+}
+
+function submitAssignment() {
+    var reqBody = {}
+    reqBody["title"] = $("#title").val();
+    reqBody["info"] = $("#info").val();
+    reqBody["minTimeSeconds"] = $("#minTimeSeconds").val();
+    reqBody["maxTimeSeconds"] = $("#maxTimeSeconds").val();
+    reqBody["startDate"] = $("#startDate").val();
+    reqBody["endDate"] = $("#endDate").val();
+    reqBody["isPublished"] = $("#isPublished").is(':checked');
+    $.ajax({
+        type : "POST",
+        contentType : "application/json",
+        url : "assignment",
+        data : JSON.stringify(reqBody),
+        timeout : 100000,
+        success : function(response) {
+            console.log("SUCCESS: ", response);
+            ReactDOM.render(<div>HEJ</div>, document.getElementById('courseContent'));
+        }, error : function(e) {
+            console.log("ERROR: ", e);
+        }, done : function(e) {
+            console.log("DONE");
+        }
+    });    
+}
+
+window.CourseContent = React.createClass({
+    render: function() {
+        var id = this.props.id;
+        var type = this.props.type;
+        var title;
+        var content = [];
+        switch(type){
+            case "course":
+                content.push(<h1>{type}</h1>);
+                break;
+            case "assignment":
+                var courseId = this.props.course;
+                var assignmentId = this.props.assignment;
+                content.push(<h2>{type} (course={courseId})</h2>);
+                content.push(<AssignmentContent course={courseId} assignment={assignmentId}/>)
+                break;
+            case "task":
+                var courseId = this.props.course;
+                var assignmentId = this.props.assignment;
+                content.push(<h3>{type} (course={courseId}) (assignment={assignmentId})</h3>);
+                break;
+            default:
+                content.push(<h4>{type}</h4>);
+                break;
+        }
+        return (
+            <div>{content}</div>
+        );
+    }
+});
+
+//ReactDOM.render(<NewAssignment />, document.getElementById('courseContent'));
+window.NewAssignment = NewAssignment;
--- Frontend_src/src/hardcodeMenu.js
+++ Frontend_src/src/hardcodeMenu.js
@@ -2,6 +2,7 @@ var CourseList = React.createClass({
     printVideo : function() {
         ReactDOM.render(<AssignmentContent course="kurs 3" assignment="assignment 2"/>,document.getElementById("courseContent"));
     },
+    
     studentSubmitions: function () {
         /* $.get("/db/getAllSubmitions",{value:1000},function (res) {
          var users = JSON.parse(res);
@@ -26,15 +27,22 @@ var CourseList = React.createClass({
             }];
         ReactDOM.render(<StudentList students={users}/>, document.getElementById('courseContent'));
     },
+
     printFeedback : function() {
         ReactDOM.render(<Feedback />,document.getElementById("courseContent"));
     },
+
+    newAssignment : function() {
+        ReactDOM.render(<NewAssignment />, document.getElementById('courseContent'));
+    },
+
 	render : function() {
 		var listComponents = [];
         listComponents.push(<div className="menuItem" key={1} onClick={this.printVideo} ><h6 className="navigationText">Video Playback</h6></div>);
         listComponents.push(<div className="menuItem" key={2} onClick={this.printFeedback}><h6 className="navigationText">Feedback</h6></div>);
         listComponents.push(<div className="menuItem" key={3}><h6 className="navigationText">Item 3</h6></div>);
         listComponents.push(<div className="menuItem" key={4} onClick={this.studentSubmitions}><h6 className="navigationText">Teacher view assignment</h6></div>);
+        listComponents.push(<div className="menuItem" key={5} onClick={this.newAssignment}><h6 className="navigationText">Teacher new assignment</h6></div>);
 		return <div>{listComponents}</div>;
 	}
 });
