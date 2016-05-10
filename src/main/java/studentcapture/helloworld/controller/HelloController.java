77a37bb35369e0aa4671c91dc1a88739a1f3484d
Zacharias <dv13zbn@cs.umu.se>
2016-05-10 13:52:43 +0200

    Revert "Removed this godamn helloworld directory"
    This reverts commit c721c40bc4a63af918938f43fff74df2f745277e.

---
+++ src/main/java/studentcapture/helloworld/controller/HelloController.java
@@ -0,0 +1,39 @@
+package studentcapture.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import studentcapture.helloworld.model.HelloModel;
+import studentcapture.helloworld.model.TestModel;
+
+@RestController
+public class HelloController {
+	private HelloModel model = new HelloModel();
+	private TestModel testModel = new TestModel();
+
+	@RequestMapping(value = "hellows", method = RequestMethod.GET)
+	public HelloModel hello() {
+		return model;
+	}
+
+	@RequestMapping(value = "hello", method = RequestMethod.POST)
+	public void hello(@RequestParam(value="greeting", required=false) String greeting) {
+		if (greeting != null) {
+			model.setGreeting(greeting);
+		}
+	}
+
+	@RequestMapping(value = "message", method = RequestMethod.GET)
+	public HelloModel msgget(@RequestParam(value="msg", required = false) String msg) {
+		model.setMessage(msg);
+		return model;
+	}
+
+	@RequestMapping(value = "message", method = RequestMethod.POST)
+	public void message(@RequestParam(value="message", required = false) String message){
+		if(message != null){
+			model.setMessage(message);
+		}
+	}
+}
