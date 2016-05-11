faf5f96d14ee1254c5bf5509632ea6d173010a79
Hampus Hedquist <c14hht@cs.umu.se>
2016-05-11 08:54:16 +0200

    Add error handling

--- Frontend_src/src/feedbackContent.js
+++ Frontend_src/src/feedbackContent.js
@@ -12,32 +12,32 @@ window.Feedback = React.createClass ({
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
                 var infoFromDb = JSON.parse(xmlHttp.responseText);
                 return (
-                    ReactDOM.render(
-                        <NewFeedback feedbackResponse={infoFromDb}
-                        sourceInfo={"https://" + window.globalURL + "/feedback/video?studentID=" + document.getElementById('student').value
-            + "&assignmentID=" + document.getElementById('ass').value + "&courseID=" + document.getElementById('course').value}/>,
-                        document.getElementById('courseContent')
-                    )
-                )
+                ReactDOM.render(
+                <NewFeedback feedbackResponse={infoFromDb}
+                sourceInfo={window.globalURL + "/feedback/video?studentID=" + document.getElementById('student').value
+                    + "&assignmentID=" + document.getElementById('ass').value + "&courseID=" + document.getElementById('course').value}/>,
+                document.getElementById('courseContent')
+            )
+            )
             }
         };
-        xmlHttp.open("GET", "https://" + window.globalURL + "/feedback/get?studentID=" + document.getElementById('student').value
+        xmlHttp.open("GET",window.globalURL + "/feedback/get?studentID=" + document.getElementById('student').value
             + "&assignmentID=" + document.getElementById('ass').value + "&courseID=" + document.getElementById('course').value, true);
         xmlHttp.send();
     },
     render: function () {
         return (
             <div>
-                <form>
-                    StudentID: <br />
-                    <input id="student"type="text" name="studentid"></input> <br />
-                    AssID: <br />
-                    <input id="ass" type="text" name="assid"></input> <br />
-                    CourseID: <br />
-                    <input id="course" type="text" name="courseid"></input>
-                </form>
-                <button style={{backgroundColor: 'black',color:'white'}} onClick={this.handleClick}>Submit</button>
-            </div>
+            <form>
+            StudentID: <br />
+        <input id="student"type="text" name="studentid"></input> <br />
+            AssID: <br />
+        <input id="ass" type="text" name="assid"></input> <br />
+            CourseID: <br />
+        <input id="course" type="text" name="courseid"></input>
+            </form>
+            <button style={{backgroundColor: 'black',color:'white'}} onClick={this.handleClick}>Submit</button>
+        </div>
         )
     }
 });
@@ -48,21 +48,30 @@ var NewFeedback = React.createClass ({
 
         var feedbackResponse = this.props.feedbackResponse;
         console.log(this.props.sourceInfo);
+        console.log(feedbackResponse.error + " feedback response");
 
-        return (
-            <div>
+        if(feedbackResponse.error === undefined) {
+            return (
+                <div>
                 Teacher: {feedbackResponse.teacher} <br />
-                Grade: {feedbackResponse.grade} <br />
-                Time: {feedbackResponse.time} <br />
-                <video width="720" height="460" src={this.props.sourceInfo} preload="auto" controls></video>
+            Grade: {feedbackResponse.grade} <br />
+            Time: {feedbackResponse.time} <br />
+            <video width="720" height="460" src={this.props.sourceInfo} preload="auto" controls></video>
             </div>
         )
+        } else {
+            return (
+                <div>
+                Error: {feedbackResponse.error} <br />
+            </div>
+        )
+        }
     }
 });
 
 /*ReactDOM.render(
-    <Feedback />,
-    document.getElementById('courseContent')
-);
-*/
+ <Feedback />,
+ document.getElementById('courseContent')
+ );
+ */
 
