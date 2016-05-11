c69e95d135b58bf90269e336a8a00b0d2008cf9f
Timmy Olsson <c12ton@cs.umu.se>
2016-05-11 15:35:10 +0200

    Added a test for h2 database

--- src/test/java/studentcapture/datalayer/database/UserTest.java
+++ src/test/java/studentcapture/datalayer/database/UserTest.java
@@ -2,25 +2,15 @@ package studentcapture.datalayer.database;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
 import studentcapture.config.StudentCaptureApplicationTests;
-import studentcapture.datalayer.database.Submission.SubmissionWrapper;
-import studentcapture.datalayer.database.User.CourseAssignmentHierarchy;
-
-import java.sql.Timestamp;
-import java.util.ArrayList;
+import static org.junit.Assert.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by tfy12hsm.
@@ -38,52 +28,48 @@ public class UserTest  extends StudentCaptureApplicationTests {
 
     @Before
     public void setUp() {
-        Mockito.reset(jdbcMock);
+        //Over writes the actual database with fake.
+        H2DataSource src = new H2DataSource();
+        jdbcMock.setDataSource(src.dataSource());
+
+
+
     }
 
+
     @Test
-    public void getCourseAssignmentHierarchyUserInformationTest() {
-    	String getUserStatement = "SELECT * FROM Users WHERE "
-        		+ "UserId=?";
-    	String getTeacherHierarchyStatement = "SELECT * FROM Participant AS par"
-				+ " LEFT JOIN Course AS cou ON par.courseId="
-	    		+ "cou.courseId LEFT JOIN Assignment AS ass ON cou.courseId="
-	    		+ "ass.courseId LEFT JOIN Submission AS sub ON "
-	    		+ "ass.assignmentId=sub.assignmentId WHERE par.userId=? AND "
-	    		+ "par.function='Teacher'";
-    	String getStudentHierarchyStatement = "SELECT * FROM "
-	    		+ "Participant AS par LEFT JOIN Course AS cou ON par.courseId="
-	    		+ "cou.courseId LEFT JOIN Assignment AS ass ON cou.courseId="
-	    		+ "ass.courseId LEFT JOIN Submission AS sub ON par.userId="
-	    		+ "sub.studentId AND ass.assignmentId=sub.assignmentId WHERE "
-	    		+ "par.userId=? AND par.function='Student'";
+    public void testAddUser() {
 
-    	Timestamp ts = new Timestamp(System.currentTimeMillis());
+       boolean res =  user.addUser("userPelle","Pelle","Jönsson","1944","mypassword123");
 
-    	Map responseFromMockUser = new HashMap();
-    	responseFromMockUser.put("UserId", 1);
-    	responseFromMockUser.put("FirstName", "nameFirst");
-    	responseFromMockUser.put("LastName", "nameLast");
-    	when(jdbcMock.queryForMap(getUserStatement, 1)).
-        		thenReturn(responseFromMockUser);
+        String sql = "SELECT * FROM users WHERE username = 'userPelle'";
 
-    	Map responseFromMockStudent = new HashMap();
-    	List<Map<String, Object>> listFromMockStudent = new ArrayList<>();
-    	listFromMockStudent.add(responseFromMockStudent);
-    	when(jdbcMock.queryForList(getUserStatement, 1)).
-			thenReturn(listFromMockStudent);
+        //Getting values from table user
+        HashMap<String,String> info = (HashMap<String,String>)
+                jdbcMock.queryForObject(sql, new UserWrapper());
 
-    	Map responseFromMockTeacher = new HashMap();
-    	List<Map<String, Object>> listFromMockTeacher = new ArrayList<>();
-    	listFromMockStudent.add(responseFromMockTeacher);
-    	when(jdbcMock.queryForList(getUserStatement, 1)).
-			thenReturn(listFromMockTeacher);
+        assertEquals("userPelle",info.get("username"));
+        assertEquals("Pelle",info.get("fName"));
+        assertEquals("Jönsson",info.get("lName"));
+        assertEquals("1944",info.get("SNN"));
+    }
 
-    	CourseAssignmentHierarchy result =
-    			user.getCourseAssignmentHierarchy("1").get();
 
-    	assertEquals(result.userId,1);
-        assertEquals(result.firstName,"nameFirst");
-        assertEquals(result.lastName,"nameLast");
+    /**
+     *  Used to collect user information, and return a hashmap.
+     */
+    protected class UserWrapper implements org.springframework.jdbc.core.RowMapper {
+
+        @Override
+        public Object mapRow(ResultSet rs, int i) throws SQLException {
+            HashMap<String,String> info = new HashMap();
+            info.put("userID",rs.getString("userid"));
+            info.put("username",rs.getString("UserName"));
+            info.put("fName",rs.getString("FirstName"));
+            info.put("lName",rs.getString("LastName"));
+            info.put("SNN",rs.getString("persnr"));
+            info.put("pswd",rs.getString("pswd"));
+            return info;
+        }
     }
 }
