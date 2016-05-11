94f51030ca5378aebad83d0da9dc240189c4bdd3
c13evk <c13ev@cs.umu.se>
2016-05-11 14:27:34 +0200

    MailClient features

--- src/main/java/studentcapture/mail/MailClient.java
+++ src/main/java/studentcapture/mail/MailClient.java
@@ -1,5 +1,15 @@
 package studentcapture.mail;
 
+
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
 /**
  * Class:       BasicSanitizer
  * <p>
@@ -8,12 +18,32 @@ package studentcapture.mail;
  * Date:        5/11/16
  */
 public class MailClient {
+    private Properties properties;
+    private Session session;
 
     public MailClient() {
 
+
+        properties = new Properties();
+        properties.put("mail.smtp.host", "mail.cs.umu.se");
+        properties.put("mail.smtp.port", "587");
+        session = Session.getDefaultInstance(properties);
+
     }
 
-    public void send() {
+    public void send(String to, String from, String subject, String msg) {
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject);
+            message.setText(msg);
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
 
     }
 }
