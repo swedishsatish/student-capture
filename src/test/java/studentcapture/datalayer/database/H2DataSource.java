ed8bbdc3912221b3307398f592bf2861ff951ae1
Timmy Olsson <c12ton@cs.umu.se>
2016-05-11 16:44:23 +0200

    Function is now static

--- src/test/java/studentcapture/datalayer/database/H2DataSource.java
+++ src/test/java/studentcapture/datalayer/database/H2DataSource.java
@@ -1,6 +1,5 @@
 package studentcapture.datalayer.database;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import studentcapture.config.StudentCaptureApplication;
@@ -9,15 +8,14 @@ import studentcapture.config.StudentCaptureApplication;
 /**
  * Created by c12ton on 5/10/16.
  */
-
 @Configuration
 public class H2DataSource {
 
-    public DriverManagerDataSource dataSource() {
+    public static DriverManagerDataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
         driverManagerDataSource.setDriverClassName("org.h2.Driver");
         String confUrl = "jdbc:h2:mem:testdb;DATABASE_TO_UPPER=FALSE;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;";
-        String pathTables = "'"+StudentCaptureApplication.ROOT+"/db/test/create-db.sql"+"'";
+        String pathTables = "'"+StudentCaptureApplication.ROOT+"/database/test/create-tables.sql"+"'";
         String initUrl = "INIT=runscript from  "+ pathTables +";";
         driverManagerDataSource.setUrl(confUrl+initUrl);
         return driverManagerDataSource;
