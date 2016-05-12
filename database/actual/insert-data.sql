295a9c5544aeef166696b97865f173382d0125e0
Timmy Olsson <c12ton@cs.umu.se>
2016-05-12 09:12:44 +0200

    Changed folder name from actual to main'

--- database/actual/insert-data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users VALUES (1, 'mkyong', 'mkyong@gmail.com');
-INSERT INTO users VALUES (2, 'alex', 'alex@yahoo.com');
-INSERT INTO users VALUES (3, 'joel', 'joel@gmail.com');
-
-INSERT INTO Course VALUES ('PVT',2016, 'VT', '1234', 'ABC');
-INSERT INTO Assignment VALUES (1, 'PVT', 'OU1', '2016-05-13 10:00:00', '2016-05-13 12:00:00', 60, 180, null, 'XYZ');
-INSERT INTO Submission VALUES (1, 1, null, '2016-05-13 11:00:00');
-
