094260bad1bccaad88d1d0ec36af8bc843e735d3
c13arm <c13arm@cs.umu.se>
2016-05-12 09:09:31 +0200

    sql queries

--- database/actual/create-db.sql
+++ database/actual/create-db.sql
@@ -3,7 +3,47 @@ CREATE TABLE IF NOT EXISTS Users (
     UserName     VARCHAR(64)    UNIQUE,
     FirstName    VARCHAR(64)    NOT NULL,
     LastName     VARCHAR(64)    NOT NULL,
-    persnr       CHAR(10)       NOT NULL,
+    PersNr       CHAR(10)       NOT NULL,
     Pswd         VARCHAR(64)    NOT NULL
     );
-
+CREATE TABLE IF NOT EXISTS Course (
+    CourseId    VARCHAR(10)    PRIMARY KEY,
+    Year        INT            NOT NULL,
+    Term        CHAR(4)        NOT NULL,
+    CourseCode  CHAR(6)        NOT NULL,
+    CourseName  VARCHAR(64)
+    );
+CREATE TYPE role AS ENUM ('Teacher', 'Student', 'Assistant');
+CREATE TABLE IF NOT EXISTS Participant (
+    UserId       INT           references Users(UserId),
+    CourseId     VARCHAR(10)   references Course(CourseId),
+    Function     role          NOT NULL
+    );
+CREATE TABLE IF NOT EXISTS Assignment (
+    AssignmentId      SERIAL         PRIMARY KEY,
+    CourseId          VARCHAR(10)    references Course(CourseId) NOT NULL,
+    Title             VARCHAR(64)    NOT NULL,
+    StartDate         timestamp      NOT NULL,
+    EndDate           timestamp      NOT NULL,
+    MinTime           INT            NOT NULL,
+    MaxTime           INT            NOT NULL,
+    Published         timestamp,
+    GradeScale        VARCHAR(64)    NOT NULL
+    );
+CREATE TABLE IF NOT EXISTS Submission (
+    AssignmentId    INT          references Assignment(AssignmentId) NOT NULL,
+    StudentId           INT          references Users(UserId) NOT NULL,
+    StudentPublishContent   Boolean ,
+    SubmissionDate      timestamp    NOT NULL,
+    Grade               VARCHAR (3),
+    TeacherId           INT          references Users(UserId),
+    PublishStudentSubmission    Boolean ,
+    PRIMARY KEY (AssignmentId, StudentId)
+    );
+CREATE TYPE lang AS ENUM ('Swedish', 'English', 'Klingon');
+CREATE TABLE IF NOT EXISTS Config (
+    UserId              INT         references Users(UserId),
+    Language            lang        NOT NULL,
+    Email               VARCHAR(100),
+    TextSize            INT         NOT NULL,
+    );
