0c2fb70e7bbd95d5a564f796670668b88adeaee4
tfy12hsm <tfy12hsm@cs.umu.se>
2016-05-12 08:44:46 +0200

    added old tables. Modify as you please.

--- database/test/create-tables.sql
+++ database/test/create-tables.sql
@@ -6,3 +6,39 @@ CREATE TABLE IF NOT EXISTS Users (
     PersNr       CHAR(10)       NOT NULL,
     Pswd         VARCHAR(64)    NOT NULL
     );
+
+CREATE TABLE IF NOT EXISTS Course (
+    CourseId    VARCHAR(10)    PRIMARY KEY,
+    Year        INT            NOT NULL,
+    Term        CHAR(4)        NOT NULL,
+    CourseCode  CHAR(6)        NOT NULL,
+    CourseName  TEXT,
+    Active      BOOLEAN        NOT NULL
+    );
+
+CREATE TYPE role AS ENUM ('Teacher', 'Student', 'Assistant');
+CREATE TABLE IF NOT EXISTS Participant (
+    UserId       INT           references Users(UserId),
+    CourseId     VARCHAR(10)   references Course(CourseId),
+    Function     role          NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS Assignment (
+    AssignmentId      SERIAL         PRIMARY KEY,
+    CourseId          VARCHAR(10)    references Course(CourseId) NOT NULL,
+    Title	          TEXT	         NOT NULL,
+    StartDate         timestamp      NOT NULL,
+    EndDate           timestamp      NOT NULL,
+    MinTime           INT            NOT NULL,
+    MaxTime           INT            NOT NULL,
+    Published         boolean        NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS Submission (
+    AssignmentId	INT          references Assignment(AssignmentId) NOT NULL,
+    StudentId           INT          references Users(UserId) NOT NULL,
+    SubmissionDate      timestamp    NOT NULL,
+    Grade               VARCHAR (3),
+    TeacherId           INT          references Users(UserId),
+    PRIMARY KEY (AssignmentId, StudentId)
+    );
