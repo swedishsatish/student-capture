373efd960e18cba6a464f536f6135fa01e9604a5
Emil Lindqvist <c13elt@cs.umu.se>
2016-05-13 14:12:06 +0200

    Added Status enum.

--- src/main/java/studentcapture/datalayer/database/Submission.java
+++ src/main/java/studentcapture/datalayer/database/Submission.java
@@ -1,64 +1,38 @@
 package studentcapture.datalayer.database;
 
-import java.sql.Timestamp;
-import java.util.Map;
 /**
  * Created by c13arm on 2016-05-11.
  */
-
 public class Submission {
-	private Integer assignmentID;
-    private Integer studentID;
-    private Boolean studentPublishConsent = false;
-    private Timestamp submissionDate;
+    private int studentID;
+    private int assignmentID;
+    private boolean studentPublishConsent = false;
     private Grade grade;
-    private Integer teacherID;
-    private Boolean publishStudentSubmission = false;
     private String courseID;
+    private Status subStatus;
 
-    public Submission() {
+    //A submission must have one of these statuses
+    public enum Status {
+        ANSWER("Answer"),
+        NOANSWER("NoAnswer"),
+        BLANK("Blank");
+
+        Status(String status) {
+        }
     }
-    
+
     public Submission (int studentID, int assignmentID) {
         this.studentID = studentID;
         this.assignmentID = assignmentID;
     }
 
-    /**
-     * Constructor that parses map of database elements.
-     * 
-     * @param map		map retrieved from database
-     * 
-     * @author tfy12hsm
-     */
-    public Submission(Map<String, Object> map) {
-    	assignmentID = (Integer) map.get("AssignmentId");
-		studentID = (Integer) map.get("StudentId");
-		try {
-			studentPublishConsent = (Boolean) map.get("StudentPublishConsent");
-		} catch (NullPointerException e) {
-		}
-		submissionDate = (Timestamp) map.get("SubmissionDate");
-		//grade = ;
-		try {
-			teacherID = (Integer) map.get("TeacherId");
-		} catch (NullPointerException e) {
-			teacherID = null;
-		}
-		try {
-			publishStudentSubmission = (Boolean) map.get("PublishStudentSubmission");
-		} catch (NullPointerException e) {
-		}
-	}
-
     public String getCourseID() {
-		return courseID;
+        return courseID;
     }
 
     public void setCourseID(String courseID) {
         this.courseID = courseID;
     }
-
     public void setGrade(Grade grade) {
         this.grade = grade;
     }
@@ -91,5 +65,13 @@ public class Submission {
 
     public void setStudentPublishConsent(boolean studentPublishConsent) {
         this.studentPublishConsent = studentPublishConsent;
+    }
+
+    public Status getSubStatus() {
+        return subStatus;
+    }
+
+    public void setSubStatus(Status status) {
+        this.subStatus = status;
     }
 }
