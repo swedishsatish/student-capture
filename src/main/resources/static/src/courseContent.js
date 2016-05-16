868a2aa8fc317cf9efceb60ac9afc730d9534f2c
Ludvig Boström <lubo0005@gapps.umu.se>
2016-05-16 07:57:03 +0200

    moved frontend code into resources

--- src/main/resources/static/src/courseContent.js
+++ src/main/resources/static/src/courseContent.js
@@ -1,87 +1,15 @@
-var NewAssignment = React.createClass({
-    formDataBuilder: function (blob, fileName) {
-        var fd = new FormData();
-        fd.append("videoName", fileName);
-        fd.append("video", blob);
-        return fd;
-    },
-    render : function() {
-      return <div>
-                <form id="form" action="assignment" method="post">
-                <input className="inputField" id="title" type="text" defaultValue="title" /><br/>
-                <input className="inputField" id="info" type="text" defaultValue="description" /><br/>
-                <input className="inputField" id="recap" type="text" defaultValue="recap" /><br/>
-                <Recorder playCallback={this.playVideo}
-                          postURL="/video/textTest" formDataBuilder={this.formDataBuilder}
-                          recButtonID="record-question" stopButtonID="stop-question" fileName="testVid.webm" replay="true"
-                          postButtonID="post-question"
-                />
-                <div className="four columns u-pull-left">
-                    <button id="record-question" className="recControls">Record</button>
-                    <button id="stop-question" className="recControls" disabled>Stop</button>
-                </div>
-                <input id="startDate" type="button" value="yyyy-mm-dd 00:00"/>Start Date<br/>
-                <input id="endDate" type="button" value="yyyy-mm-dd 00:00"/>End Date<br/>
-                <input id="minTimeSeconds" type="number" defaultValue="minTimeSeconds" /><br/>
-                <input id="maxTimeSeconds" type="number" defaultValue="maxTimeSeconds" /><br/>
-                <input id="publish" type="button" value="yyyy-mm-dd 00:00"/>Publish Date<br/>
-                    <select id="scale">
-                        <option value="NUMBER_SCALE">1,2,3,4,5</option>
-                        <option value="U_G_VG_MVG">U,G,VG,MVG</option>
-                        <option value="U_O_K_G">U,O,K,G</option>
-                    </select>Grade scale<br/>
-                <div className="button primary-button" onClick = {handleCancel}> CANCEL </div>
-                <div className="button primary-button" id="post-question" onClick = {submitAssignment}> SUBMIT </div>
-            </form>
-        </div>
-    },
 
-    componentDidMount: function () {
-        $("#startDate").datetimepicker(
-            {
-                dateFormat: "yy-mm-dd",
-                /*
-                 minDate
-                 jQuery datepicker option
-                 which set today date as minimum date
-                 */
-                minDate: 0
-            });
-        $("#endDate").datetimepicker(
-            {
-                dateFormat: "yy-mm-dd",
-                /*
-                 minDate
-                 jQuery datepicker option
-                 which set today date as minimum date
-                 */
-                minDate: 0
-            });
-        $("#publish").datetimepicker(
-            {
-                dateFormat: "yy-mm-dd",
-                /*
-                 minDate
-                 jQuery datepicker option
-                 which set today date as minimum date
-                 */
-                minDate: 0
-            });
+var StartPage = React.createClass({
+    render: function () {
+        return <div>
+            <h3>Welcome to Student Capture!</h3>
+            <p>A video examination platform</p>
+        </div>
     }
+
 });
 
-function handleCancel() {
-
-}
-
 function submitAssignment() {
-    var ReturnMessage = React.createClass({
-        render : function() {
-            return (
-                <div>{this.props.message} {this.props.response}</div>
-            )
-        }
-    });
     var reqBody = {}
     reqBody["title"] = $("#title").val();
     reqBody["info"] = $("#info").val();
@@ -89,71 +17,54 @@ function submitAssignment() {
     reqBody["maxTimeSeconds"] = $("#maxTimeSeconds").val();
     reqBody["startDate"] = $("#startDate").val();
     reqBody["endDate"] = $("#endDate").val();
-    reqBody["published"] = $("#publish").val();
-    reqBody["recap"] = $("#recap").val();
-    reqBody["scale"] = $("#scale").val();
+    reqBody["isPublished"] = $("#isPublished").is(':checked');
     $.ajax({
         type : "POST",
         contentType : "application/json",
         url : "assignment",
         data : JSON.stringify(reqBody),
         timeout : 100000,
-        success : function(responseIn) {
-            console.log("SUCCESS: ", responseIn);
-            ReactDOM.render(<VideoComponent assignmentID={responseIn}/>, document.getElementById('courseContent'));
-        }, error : function(jqxhr) {
-            console.log("ERROR: ", jqxhr);
-            ReactDOM.render(<ReturnMessage message="ERROR" reponse=""/>, document.getElementById('courseContent'));
-       }, done : function(e) {
+        success : function(response) {
+            console.log("SUCCESS: ", response);
+            ReactDOM.render(<div>HEJ</div>, document.getElementById('courseContent'));
+        }, error : function(e) {
+            console.log("ERROR: ", e);
+        }, done : function(e) {
             console.log("DONE");
         }
-    });
+    });    
 }
-
-var VideoComponent = React.createClass ({
-    render: function() {
-        return (
-            <div>
-                <Recorder playCallback={this.playVideo}
-                                 postURL="/uploadVideo/" formDataBuilder={this.formDataBuilder}
-                                 recButtonID="record-question" stopButtonID="stop-question" fileName="testVid.webm" replay="true"
-                                 postButtonID="post-question"
-                />
-                <div className="four columns u-pull-left">
-                    <button id="record-question" className="recControls">Record</button>
-                    <button id="stop-question" className="recControls" disabled>Stop</button>
-                </div>
-            </div>
-        )
-    }
-});
 
 window.CourseContent = React.createClass({
     render: function() {
         var id = this.props.id;
         var type = this.props.type;
         var title;
+        var content = [];
         switch(type){
             case "course":
-                title = <h1>{type}</h1>
+                var course = this.props.course;
+                content.push(<h1>{type} {course["name"]}</h1>);
                 break;
             case "assignment":
-                title = <h2>{type}</h2>
+                var courseId = this.props.course;
+                var assignmentId = this.props.assignment;
+                content.push(<h2>{type} (course={courseId})</h2>);
+                content.push(<AssignmentContent course={courseId} assignment={assignmentId}/>)
                 break;
             case "task":
-                title = <h3>{type}</h3>
+                var courseId = this.props.course;
+                var assignmentId = this.props.assignment;
+                content.push(<h3>{type} (course={courseId}) (assignment={assignmentId})</h3>);
                 break;
             default:
-                title = <h4>{type}</h4>
+                content.push(<h4>{type}</h4>);
                 break;
         }
         return (
-            <div>
-                {title}
-                ID: {id}
-            </div>
+            <div>{content}</div>
         );
     }
 });
 
-ReactDOM.render(<NewAssignment />, document.getElementById('courseContent'));
+ReactDOM.render(<StartPage />, document.getElementById('courseContent'));
---
+++ src/main/resources/static/src/g6/CheckBox.js
@@ -0,0 +1,45 @@
+/**
+ * Created by sejiaw on 2016-05-10.
+ */
+
+
+/**
+ * Checkbox that is used to determine if a student passed an assignment or not.
+ * @type {number} current state of passed
+ */
+var CheckBox = React.createClass({
+    //Sets initial state for the checkbox to false.
+    getInitialState: function() {
+        return {
+
+            checkBox: this.props.checkBox || false
+
+        };
+    },
+    // Render function for CheckBox
+    render: function() {
+        return (
+
+            <div id="checklabel">
+                <label for="checklabel">
+                    <input id="ifStudentPass" type="checkbox"
+                           checkBox={this.state.checkBox}
+                           onClick={this.handleClick}/>
+
+                    Passed!
+                </label>
+
+            </div>
+        );
+    },
+    //Changes from checked to unchecked and vice-versa.
+    handleClick: function(e) {
+
+        this.setState({
+            checkBox: e.target.checkBox
+        });
+
+    }
+});
+
+window.CheckBox=CheckBox;
