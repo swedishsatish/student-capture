5742f0394e57e0e45387dc4da2effb1e8b0ec2f7
DavidBjorkstrand <tfy13dbd@cs.umu.se>
2016-05-17 09:38:48 +0200

    Added better tests.

--- src/main/resources/static/src/g1/newAssignment.js
+++ src/main/resources/static/src/g1/newAssignment.js
@@ -141,5 +141,5 @@ window.CourseContent = React.createClass({
     }
 });
 
-//ReactDOM.render(<NewAssignment />, document.getElementById('courseContent'));
+ReactDOM.render(<NewAssignment />, document.getElementById('courseContent'));
 window.NewAssignment = NewAssignment;
