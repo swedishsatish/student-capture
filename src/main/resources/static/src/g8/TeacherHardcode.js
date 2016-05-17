a5afd45188d16acdb63034952f8c826858869b66
Tobias Estefors <dv13tes@cs.umu.se>
2016-05-17 13:19:08 +0200

    Hardcoded some studentlist for testing

---
+++ src/main/resources/static/src/g8/TeacherHardcode.js
@@ -0,0 +1,55 @@
+/**
+ *
+ *@author: Tobias Estefors <dv13tes>
+ **/
+
+var TeacherViewSubmission=React.createClass({
+    submissions:0,
+    withdrawls:0,
+    done:0,
+    amount:0,
+
+
+    componentDidMount: function() {
+
+
+    },
+    calculateSubmissions: function () {
+        var userList=window.users;
+        for(var i=0; i < userList.length;i++){
+            console.log(userList[i].studentName.value)
+            if(userList[i].withdraw){
+                this.withdrawls++;
+                continue;
+            }
+            this.submissions++;
+        }
+        this.done=this.submissions+this.withdrawls;
+        this.amount=userList.length;
+    },
+
+    render:function () {
+        this.calculateSubmissions();
+        return (
+            <div class="row">
+                <div className="four columns offset-by-one">
+
+                    <div id="studentContainer">
+                        <StudentList/>
+                        <div id="textList">
+                            <p>Submissions: {this.submissions}/{this.amount}</p>
+                            <p>Withdrawl: {this.withdrawls}</p>
+                            <p>Done: {this.done}/{this.amount}</p>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        )
+    }
+
+
+});
+
+window.TeacherViewSubmission=TeacherViewSubmission;
