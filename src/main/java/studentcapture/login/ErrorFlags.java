931548be587a47cf16502d7888dbe91a0a9b137d
Erik Andersson <dv13ean@cs.umu.se>
2016-05-19 09:05:43 +0200

    Created enum for error message communication between db and spring

---
+++ src/main/java/studentcapture/login/ErrorFlags.java
@@ -0,0 +1,13 @@
+package studentcapture.login;
+
+/**
+ * @author dv13ean
+ */
+public enum ErrorFlags {
+    USERNAMELENGTH,
+    PASSWORDFORMAT,
+    PASSWORDMATCH,
+    EMAILFORMAT,
+    EMAILEXISTS,
+    USEREXISTS
+}
