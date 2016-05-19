3d72fa8f35f654c01d90bedc0c411d8057ebf4b8
HBylund <c13hbd@cs.umu.se>
2016-05-19 10:26:16 +0200

    Correct error messages on registration failure

--- src/main/java/studentcapture/login/ErrorFlags.java
+++ src/main/java/studentcapture/login/ErrorFlags.java
@@ -1,16 +1,63 @@
 package studentcapture.login;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import studentcapture.user.User;
+
 /**
  * Enum for sending error flags between the db and spring.
  * Used during login.
  * @author dv13ean
  */
 public enum ErrorFlags {
-    USERNAMELENGTH, //Username length is invalid
-    PASSWORDFORMAT, //Password does not follow the format
-    PASSWORDMATCH,  //The two given passwords do not match
-    EMAILFORMAT,    //The given email is not on a valid format
-    EMAILEXISTS,    //The given email is already in the database
-    USEREXISTS,     //The given username is already in the database
-    NOERROR         //No error
+    USERNAMELENGTH,
+    PASSWORDFORMAT,
+    PASSWORDMATCH,
+    EMAILFORMAT,
+    EMAILEXISTS,
+    USEREXISTS,
+    NOERROR;
+    
+    @Override
+    public String toString(){
+        
+        switch(this){
+        case USERNAMELENGTH:
+            return "usernamelength";
+            
+        case PASSWORDFORMAT:
+            return "passwordformat";
+            
+        case PASSWORDMATCH:
+            return "passwordmatch";
+            
+        case EMAILFORMAT:
+            return "emailformat";
+            
+        case EMAILEXISTS:
+            return "emailexists";
+            
+        case USEREXISTS:
+            return "userexists";
+            
+        case NOERROR:
+            return "success";
+            
+        default:
+            return "success";
+
+        }
+        
+    }
+    
 }
+
