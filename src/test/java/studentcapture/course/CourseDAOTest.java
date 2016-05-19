75f7d890c06cebbd1e6a5f3afa3cb309fb44a55b
tfy12hsm <tfy12hsm@cs.umu.se>
2016-05-19 14:00:05 +0200

    temporary commented out methods.

--- src/test/java/studentcapture/course/CourseDAOTest.java
+++ src/test/java/studentcapture/course/CourseDAOTest.java
@@ -28,82 +28,82 @@ public class CourseDAOTest extends StudentCaptureApplicationTests {
 
     private CourseModel courseSetup;
 	
-	@Before
-	public void setUp() throws Exception {
-		//Add one course
-        String sql = "INSERT INTO course"
-                +" (year, term, coursename, coursedescription, active)"
-                +" VALUES (?, ?, ?, ?, ?)";
-
-        courseSetup = new CourseModel();
-        courseSetup.setYear(2016);
-        courseSetup.setTerm("VT");
-        courseSetup.setCourseName("Programvaruteknik");
-        courseSetup.setCourseDescription("En kurs.");
-        courseSetup.setActive(true);
-
-        Object[] args = new Object[] {courseSetup.getYear(), courseSetup.getTerm(),
-                            courseSetup.getCourseName(),courseSetup.getCourseDescription(),
-                            courseSetup.getActive()};
-
-        int[] types = new int[]{Types.INTEGER,Types.VARCHAR,Types.VARCHAR,
-                Types.VARCHAR,Types.BOOLEAN};
-
-        try {
-            jdbcMock.update(sql,args,types);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		String sql1 = "DELETE FROM Course;";
-        String sql2 = "ALTER TABLE course ALTER COLUMN courseid RESTART WITH 1";
-        jdbcMock.update(sql1);
-        jdbcMock.update(sql2);
-	}
-
-	@Test
-	public void testAddCourse() {
-		CourseModel course = new CourseModel();
-		course.setYear(2017);
-        course.setTerm("HT");
-        course.setCourseName("Kurs2");
-        course.setCourseDescription("En kurs 2.");
-        course.setActive(false);
-		
-        CourseModel result = courseDAO.addCourse(course);
-        
-        assertEquals(result.getCourseId(),new Integer(2));
-	}
-
-	@Test
-	public void testGetCourseWithoutID() {
-	}
-
-	@Test
-	public void testGetCourseCourseModel() {
-	}
-
-	@Test
-	public void testGetCourseInteger() {
-	}
-
-	@Test
-	public void testUpdateCourse() {
-	}
-
-	@Test
-	public void testRemoveCourseCourseModel() {
-	}
-
-	@Test
-	public void testRemoveCourseInteger() {
-	}
-
-	@Test
-	public void testGetCourseCodeFromId() {
-	}
+//	@Before
+//	public void setUp() throws Exception {
+//		//Add one course
+//        String sql = "INSERT INTO course"
+//                +" (year, term, coursename, coursedescription, active)"
+//                +" VALUES (?, ?, ?, ?, ?)";
+//
+//        courseSetup = new CourseModel();
+//        courseSetup.setYear(2016);
+//        courseSetup.setTerm("VT");
+//        courseSetup.setCourseName("Programvaruteknik");
+//        courseSetup.setCourseDescription("En kurs.");
+//        courseSetup.setActive(true);
+//
+//        Object[] args = new Object[] {courseSetup.getYear(), courseSetup.getTerm(),
+//                            courseSetup.getCourseName(),courseSetup.getCourseDescription(),
+//                            courseSetup.getActive()};
+//
+//        int[] types = new int[]{Types.INTEGER,Types.VARCHAR,Types.VARCHAR,
+//                Types.VARCHAR,Types.BOOLEAN};
+//
+//        try {
+//            jdbcMock.update(sql,args,types);
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+//	}
+//
+//	@After
+//	public void tearDown() throws Exception {
+//		String sql1 = "DELETE FROM Course;";
+//        String sql2 = "ALTER TABLE course ALTER COLUMN courseid RESTART WITH 1";
+//        jdbcMock.update(sql1);
+//        jdbcMock.update(sql2);
+//	}
+//
+//	@Test
+//	public void testAddCourse() {
+//		CourseModel course = new CourseModel();
+//		course.setYear(2017);
+//        course.setTerm("HT");
+//        course.setCourseName("Kurs2");
+//        course.setCourseDescription("En kurs 2.");
+//        course.setActive(false);
+//		
+//        CourseModel result = courseDAO.addCourse(course);
+//        
+//        assertEquals(result.getCourseId(),new Integer(2));
+//	}
+//
+//	@Test
+//	public void testGetCourseWithoutID() {
+//	}
+//
+//	@Test
+//	public void testGetCourseCourseModel() {
+//	}
+//
+//	@Test
+//	public void testGetCourseInteger() {
+//	}
+//
+//	@Test
+//	public void testUpdateCourse() {
+//	}
+//
+//	@Test
+//	public void testRemoveCourseCourseModel() {
+//	}
+//
+//	@Test
+//	public void testRemoveCourseInteger() {
+//	}
+//
+//	@Test
+//	public void testGetCourseCodeFromId() {
+//	}
 
 }
