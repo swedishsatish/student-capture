c37b69de97b40cc020eaac1c8a4d0b0e57d58124
Alexander Jakobsson <tfy12ajn@cs.umu.se>
2016-05-26 09:45:50 +0200

    Added comments.

--- src/main/java/studentcapture/help/HelpResource.java
+++ src/main/java/studentcapture/help/HelpResource.java
@@ -9,45 +9,55 @@ import studentcapture.administrator.AdministratorDAO;
 import studentcapture.mail.MailClient;
 
 /**
- * Created by g8 on 2016-05-24.
+ * Rest controller for help related functions like FAQ, contact form.
  *
+ * Created by g8 on 2016-05-24.
  */
 @RestController
 @RequestMapping(value = "/help")
 public class HelpResource {
 
+    /** Map the administratorDAO variable to the bean. */
     @Autowired
     AdministratorDAO administratorDAO;
 
+    /*
+     * note:
+     *
+     * Multiple files using the same @Autowired but only one
+     * bean is created (?).
+     * Could be fixed with @Qualifier to distinguish between
+     * different beans of the same class, but then more beans
+     * will be created.
+     */
     //@Autowired
-    //@Qualifier("helpMailClient")
+    //@Qualifier("someIdentifier")
+    /** Temporary solution for mail client dependency. */
     private MailClient mailClient = new MailClient();
 
     /**
-     * Get the FAQ.
-     * @return ?
+     * Should get the FAQ as html file and return to the front-end.
+     * @return the FAQ.
      */
+    @Deprecated
     @CrossOrigin
     @RequestMapping(
             value = "/faq",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public String getFAQ() {
-        return "FAQ sample text";
+    public ResponseEntity<String> getFAQ() {
+        return new ResponseEntity<>("FAQ sample text", null);
     }
-
 
     /**
      * Take the contact form and email it to the administrator email
-     * that is located in the DB.
-     *
-     * IMPORTANT:
-     * No guarantee at the moment that a real email is located in the
-     * database, therefore a temp email is used to develop.
+     * that is located in the database.
+     * The sender "nobody@cs.umu.se" is used because according to
+     * support@cs it will be thrown in the spam folder directly.
      *
      * @param scContactForm Form information.
-     * @return              200 OK (but does not guarantee that the mail is sent),
+     * @return              200 OK,
      *                      500 Internal Server Error.
      */
     @CrossOrigin
@@ -63,28 +73,24 @@ public class HelpResource {
         String subject = "Student Capture Contact Form";
         String message;
 
-
-        // See method doc.
+        /* Get the email address for the receiver */
         to = administratorDAO.getSupportEmail();
         if (to == null) {
-            return new ResponseEntity<String>(
-                    "No administrator mail found",
+            return new ResponseEntity<>(
+                    "No administrator mail found.",
                     HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-
-        /* Temp email.
-        to = "c13bbd@cs.umu.se";
-        */
-
+        /* Format the message and send the email */
         message = formatMessage(scContactForm);
         mailClient.send(to, from, subject, message);
-        return new ResponseEntity<>("", HttpStatus.OK);
+
+        return new ResponseEntity<>("Mail sent.", HttpStatus.OK);
     }
 
     /**
-     * Make it easier to edit the form to email and the above
-     * easier to read.
+     * Make it easier to edit the form message and the calling
+     * method easier to read.
      * @param scContactForm Form information.
      * @return The message to email.
      */
@@ -96,4 +102,5 @@ public class HelpResource {
                 + "---\n"
                 + scContactForm.getSenderMessage();
     }
+
 }
